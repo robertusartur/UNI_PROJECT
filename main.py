eda881be7795a573b5c69b15ec5fa0b674c6b085
robertusartur <160214837+robertusartur@users.noreply.github.com>
2024-06-04 20:57:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -244,36 +244,36 @@ class NotificationResponse(BaseModel):
     read: bool
 
 class PredictParams(BaseModel):
-   datetime: str
-   geo_lat: float
-   geo_lon: float
-   building_type: int
-   level: int
-   levels: int
-   rooms: int
-   area: float
-   kitchen_area: float
-   object_type: int
-   studio_apartment: int
-   region: str
+    datetime: str
+    geo_lat: float
+    geo_lon: float
+    building_type: int
+    level: int
+    levels: int
+    rooms: int
+    area: float
+    kitchen_area: float
+    object_type: int
+    studio_apartment: int
+    region: str
 
-class Config:
-   schema_extra = {
-"example": {
-"datetime": "2021-08-01",
-"geo_lat": 59.805808,
-"geo_lon": 30.376141,
-"building_type": 1,
-"level": 1,
-"levels": 8,
-"rooms": 3,
-"area": 82.6,
-"kitchen_area": 10.8,
-"object_type": 1,
-"studio_apartment": 0,
-"region": "Санкт-Петербург"
-}
-}
+    class Config:
+        schema_extra = {
+            "example": {
+                "datetime": "2021-08-01",
+                "geo_lat": 59.805808,
+                "geo_lon": 30.376141,
+                "building_type": 1,
+                "level": 1,
+                "levels": 8,
+                "rooms": 3,
+                "area": 82.6,
+                "kitchen_area": 10.8,
+                "object_type": 1,
+                "studio_apartment": 0,
+                "region": "Санкт-Петербург"
+            }
+        }
 # Utility functions
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
